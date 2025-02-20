9998600a8fb5ba37577fcce524f36c27594b35fe
Artemis Livingstone <ar.d.livingstone@gmail.com>
2025-02-20 04:37:03 +0000

    add easter egg

--- assets/js/main_2025.js
+++ assets/js/main_2025.js
@@ -1,3 +1,13 @@
+function bytes(length) {
+    let hexLine = '';
+    for (let i = 0; i < length; i++) {
+        hexLine += '0x' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0') + ' ';
+    }
+    return hexLine;
+}
+
+var byte_count = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
     new SweetScroll({});
 
@@ -15,4 +25,27 @@ document.addEventListener("DOMContentLoaded", function () {
         backgroundColor: 0x292a2d,
         color: 0x996D02,
     });
+
+    var overflow = document.getElementById("overflow");
+
+    window.addEventListener('scroll', function () {
+        setTimeout(() => {
+            memLeak(5);
+        }, !1);
+    });
+
+
+    function memLeak(num) {
+        if (window.scrollY + window.innerHeight > document.body.offsetHeight - 100) {
+            if (byte_count >= 100) {
+                document.getElementById("segfault").style.visibility = "visible";
+                return;
+            }
+            byte_count += 1;
+            for (var i = 0; i < num; i++) {
+                overflow.innerHTML += bytes(25);
+            }
+        }
+    }
+
 }, !1);
