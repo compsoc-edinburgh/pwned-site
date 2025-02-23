91deb610b79a9f520e0d8f1118c448f7dc2b5f06
Artemis Livingstone <ar.d.livingstone@gmail.com>
2025-02-23 18:50:24 +0000

    deploy discount ticket code challenge

--- assets/js/main_2025.js
+++ assets/js/main_2025.js
@@ -1,9 +1,27 @@
+---
+---
+
+let hexStream = '';
+
 function bytes(length) {
     let hexLine = '';
     for (let i = 0; i < length; i++) {
         hexLine += '0x' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0') + ' ';
     }
     return hexLine;
+}
+
+function readStreamFromURL(url) {
+    return fetch(url)
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            let hexStream = '';
+            const byteArray = new Uint32Array(buffer);
+            byteArray.forEach(byte => {
+                hexStream += '0x' + byte.toString(16).padStart(8, '0') + ' ';
+            });
+            return hexStream;
+        });
 }
 
 var byte_count = 0;
@@ -28,22 +46,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var overflow = document.getElementById("overflow");
 
-    window.addEventListener('scroll', function () {
-        setTimeout(() => {
-            memLeak(5);
-        }, !1);
-    });
 
+
+    readStreamFromURL('{{ site.baseurl }}/assets/discount_challenges/dump').then(stream => {
+        hexStream = stream;
+    }).then(() => {
+        window.addEventListener('scroll', function () {
+            setTimeout(() => {
+                memLeak(5);
+            }, !1);
+        });
+    });
 
     function memLeak(num) {
         if (window.scrollY + window.innerHeight > document.body.offsetHeight - 100) {
-            if (byte_count >= 100) {
+            if (byte_count >= hexStream.split(' ').length / 25) {
                 document.getElementById("segfault").style.visibility = "visible";
                 return;
             }
+
             byte_count += 1;
             for (var i = 0; i < num; i++) {
-                overflow.innerHTML += bytes(25);
+                overflow.innerHTML += hexStream.split(' ').slice(byte_count * 25, (byte_count + 1) * 25).join(' ') + ' ';
             }
         }
     }
